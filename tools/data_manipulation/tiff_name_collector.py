f802d06f90da25ef844937e826e3ef1ec6ecd319
JC Steenkamp <chris.steenkamp@live.com>
2025-11-12 12:59:46 +0200

    Updates to the tiff_name_collector.py script

--- tools/data_manipulation/tiff_name_collector.py
+++ tools/data_manipulation/tiff_name_collector.py
@@ -1,21 +1,22 @@
 import os
+import sys
 import pandas as pd
 from tkinter import Tk, filedialog, messagebox
 
 # Hide the root window
-TK().withdraw()
+Tk().withdraw()
 
 # Ask user to select the main folder
-main_folder = filedialog.askdirectory(title="Hey Jana, please select the Main Folder to Search")
+main_folder = filedialog.askdirectory(title="Please select the folder (Institution) where your .tiff files are stored by collection name or abbreviation")
 if not main_folder:
         messagebox.showinfo("Cancelled", "No folder selected. Exiting")
-        exit()
+        sys.exit()
 
 data = [] 
 
 # Walk through folders recursively
 for root, dirs, files in os.walk(main_folder):
-        for files in files:
+        for file in files:
                 if file.lower().endswith(('.tiff')):
                         folder_name = os.path.basename(root)
                         data.append({
@@ -26,11 +27,11 @@ for root, dirs, files in os.walk(main_folder):
 
 if not data:
         messagebox.showinfo("Done", "No TIFF files found in the selected folder.")
-        exit() 
+        sys.exit() 
 
 # Save to excel
 df = pd.DataFrame(data)
-output_path = os.path,join(main_folder, "tiff_files_list.xlsx",)
+output_path = os.path.join(main_folder, "tiff_files_list.xlsx",)
 df.to_excel(output_path, index=False)
 
-messagebox.showinfo("Success", "Excel file created:\n\n{output_path}")
+messagebox.showinfo("Success", f"Excel file created:\n\n{output_path}")
